02a158f00060928914322721b659d2ab42fa3e0d
C20071 <75099111+C20071@users.noreply.github.com>
2021-04-04 11:21:05 -0400

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -14,27 +14,19 @@ document.getElementById("player_question").innerHTML= "Question Turn ~" + player
 document.getElementById("player_answer").innerHTML= "Answer Turn ~" + player2_name;
 
 function send(){
-    get_word= document.getElementById("word").value;
-    word= get_word.toLowerCase();
+    number1= document.getElementById("number1").value;
+    number2= document.getElementById("number2").value;
+    actual_answer=parseInt(number1)*parseInt(number2);
 
-    charAt1= word.charAt(1);
 
-    length_divide_2= word.Math.floor(word.length/2);
-    charAt2= word.charAt(length_divide_2);
-
-    length_minus_1= word.length-1;
-    charAt3= word.charAt(length_minus_1);
-
-    remove_charAt1=word.replace(charAt1,"_");
-    remove_charAt2=charAt1.replace(charAt2,"_");
-    remove_charAt3=charAt2.replace(charAt3,"_");
-
-    question_word= "<h4 id='word_display'> Q."+remove_charAt3+"</h4>";
+    question_number= "<h4 id='number_display'> Q."+number1+"x"+number2+"</h4>";
     input_box="<br> Answer: <input type='text' id='input_check_box>";
     check_button="<br> <br> <button class='btn btn-info' onclick='check()'> Check </button>";
     row= question_word+input_box+check_button;
     document.getElementById("output").innerHTML=row;
-document.getElementById("word").value="";
+document.getElementById("number1").value="";
+document.getElementById("number2").value="";
+
 }
 
 question_turn="player_1";
